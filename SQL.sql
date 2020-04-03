0b16493cce18d79cce88e8bd8f3db34119f0fadc
MartaSuc <62238657+MartaSuc@users.noreply.github.com>
2020-04-03 12:50:19 +0200

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,4 +1,4 @@
-//answer 1.1
+--answer 1.1
 SELECT b.Name FROM Building b
 JOIN City ON
 b.CityID=City.CityID
@@ -6,7 +6,7 @@ JOIN Country c ON
 City.CountryID=c.CountryID
 WHERE Country.Name='Poland';
 
-//answer 1.2
+--answer 1.2
 SELECT Country.Name FROM Country
 JOIN City ON
 Country.CountryID=City.CountryID
@@ -18,7 +18,7 @@ GROUP BY CountryID
 HAVING SUM(Population)>400;
 */
 
-//answer 1.3*
+--answer 1.3*
 SELECT Country.Name FROM Country
 JOIN City ON
 Country.CountryID=City.CountryID
